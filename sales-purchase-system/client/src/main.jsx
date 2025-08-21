23ec6636efd2977a4bcdbd97cd10c2dca9ff458a
hiselvatest2026-ux <hiselvatest2026@gmail.com>
2025-08-22 02:42:20 +0530

    modified file

--- sales-purchase-system/client/src/main.jsx
+++ sales-purchase-system/client/src/main.jsx
@@ -1,10 +1,12 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
